6499b92f1a3ee44800132d3f1d44963cf3419c1e
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-14 20:43:06 +0500

    add drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { env } from "./src/schemas/env-schema";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./src/db/mirgrations",
+  schema: "./src/db/schemas",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.AUTH_DRIZZLE_URL,
+  },
+  verbose: true,
+  strict: true,
+});
