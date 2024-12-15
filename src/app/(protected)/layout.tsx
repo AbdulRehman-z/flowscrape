dd08c037d710291e76d0bec6d9ef60bd505445c9
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-15 21:11:15 +0500

    modify layout to deal with react-query

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,5 +1,6 @@
-import { AppSidebar } from "@/components/app-sidebar";
+import { AppSidebarWrapper } from "@/components/app-sidebar-wrapper";
 import SideBarBreadCrumb from "@/components/bread-crumb";
+import QueryClientProvider from "@/components/query-client-provider";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/toggle-theme";
 import { Separator } from "@/components/ui/separator";
@@ -11,27 +12,29 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 justify-between px-5 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-5 " />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <SideBarBreadCrumb />
-            </div>
-            <ModeToggle />
-          </header>
-          <Separator />
-          <main className="flex pt-3 pb-4 px-4  flex-1 overflow-hidden">{children}</main>
-        </SidebarInset>
-      </SidebarProvider>
-    </ThemeProvider>
+    <QueryClientProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <SidebarProvider>
+          <AppSidebarWrapper />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 justify-between px-5 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-5 " />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <SideBarBreadCrumb />
+              </div>
+              <ModeToggle />
+            </header>
+            <Separator />
+            <main className="flex pt-12 pb-4 pl-16 overflow-hidden">{children}</main>
+          </SidebarInset>
+        </SidebarProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
