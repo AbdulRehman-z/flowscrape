09a4870b04523a98aa261b79553fcf2e8f39b218
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-15 21:13:57 +0500

    define workflow table schema

---
+++ src/db/schemas/workflow-schema.ts
@@ -0,0 +1,22 @@
+import { pgTable, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core";
+
+export const workflow = pgTable(
+  "workflow",
+  {
+    id: text("id").primaryKey().$defaultFn(() => crypto.randomUUID()),
+    userId: text("userId").notNull(),
+    name: text("name").notNull(),
+    description: text("description"),
+    defination: text("defination").notNull(),
+    status: text("status").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .notNull()
+      .$defaultFn(() => new Date()),
+    updatedAt: timestamp("updated_at", { withTimezone: true })
+      .notNull()
+      .$defaultFn(() => new Date()),
+  },
+  (workflow) => ({
+    userIdNameIdx: uniqueIndex("userId_name_idx").on(workflow.userId, workflow.name),
+  })
+);
