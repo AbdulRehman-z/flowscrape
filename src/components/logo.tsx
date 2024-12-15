f638332d1547e2ff50f027e5e5d31d62bf8e2e75
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-15 21:12:25 +0500

    modify logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,22 +1,21 @@
 "use client"
 
-import { ChevronRight } from "lucide-react"
-import { SidebarHeader, SidebarMenu, SidebarMenuItem, SidebarMenuButton } from "./ui/sidebar"
+import { SidebarMenu, SidebarMenuItem } from "./ui/sidebar"
 
 export function Logo() {
   return (
     <SidebarMenu>
-      <SidebarMenuItem>
-        <SidebarMenuButton size="lg">
-          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-            <ChevronRight className="size-4" />
+      <SidebarMenuItem className="flex mx-auto py-2">
+        <div className="flex items-center gap-2">
+          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary">
+            <div className="size-5 bg-white rounded-md"></div>
           </div>
-          <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-semibold">
-              Logo
+          <div className="grid flex-1 text-left text-lg leading-tight">
+            <span className="truncate font-semibold ">
+              Acme Inc
             </span>
           </div>
-        </SidebarMenuButton>
+        </div>
       </SidebarMenuItem>
     </SidebarMenu>
   )
