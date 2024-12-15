d16a9c0a9d62837ed62dbee113de5caa55e53708
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-15 21:10:54 +0500

    add workflow action

---
+++ src/actions/workflow/get-workflow-action.ts
@@ -0,0 +1,18 @@
+import { auth } from "@/auth";
+import { db } from "@/db";
+import { workflow } from "@/db/schemas/workflow-schema";
+import { eq } from "drizzle-orm";
+
+export const getUserWorkflowsAction = async () => {
+  try {
+    const session = await auth();
+    if (!session?.user?.id) {
+      throw new Error("User not authenticated");
+    }
+    const workflows = await db.select().from(workflow).where(eq(workflow.userId, session.user.id));
+    return workflows
+  } catch (error) {
+    console.log(error);
+    throw new Error("Something went wrong!", { cause: error });
+  }
+};
