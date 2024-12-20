c88b1547aa01f691f6f86266a7c70aaec264f463
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-20 21:03:21 +0500

    add defaault node

--- src/actions/workflow/create-workflow-action.ts
+++ src/actions/workflow/create-workflow-action.ts
@@ -2,9 +2,13 @@
 
 import { auth } from "@/auth";
 import { db } from "@/db";
-import { workflow } from "@/db/schemas/workflow-schema";
+import { workflows } from "@/db/schemas/workflow-schema";
+import { createFlowNode } from "@/lib/workflow/create-flow-node";
 import { CreateWorkflowSchemaType } from "@/schemas/workflow-schema";
+import { AppNodeType } from "@/types/app-node-types";
+import { TaskTypeEnum } from "@/types/task-type";
 import { WorkflowStatus } from "@/types/workflow-types";
+import { Edge } from "@xyflow/react";
 import { redirect } from "next/navigation";
 
 export const createWorkflowAction = async (
@@ -12,23 +16,27 @@ export const createWorkflowAction = async (
 ) => {
   const session = await auth();
   if (!session?.user?.id) {
-    return {
-      success: false,
-      error: "User not authenticated",
-    };
+    throw new Error("Unauthorized");
   }
+
+
+  const initialFlow: { nodes: AppNodeType[], edges: Edge[] } = {
+    nodes: [],
+    edges: []
+  }
+  initialFlow.nodes.push(createFlowNode(TaskTypeEnum.LAUNCH_BROWSER))
 
   let result
 
   try {
     [result] = await db
-      .insert(workflow)
+      .insert(workflows)
       .values({
         userId: session.user.id,
         name: data.name,
         description: data.description,
         status: WorkflowStatus.DRAFT,
-        defination: "TODO",
+        defination: JSON.stringify(initialFlow),
       })
       .returning();
 
