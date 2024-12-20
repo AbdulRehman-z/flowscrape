c33dde3f5ba27c39db97395097c0b042ce519900
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-20 21:05:42 +0500

    modify dashboard

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Page() {
   return (
-    <section className="flex flex-1 flex-col gap-4 section-padding ">
+    <section className="flex flex-1 flex-col gap-4 lg:section-padding md:px-4 pb-2 py-4   ">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
