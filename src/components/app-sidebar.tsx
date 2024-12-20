eee58e377d66f696bb5aa37531ecce62e1aab605
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-20 21:06:42 +0500

    remove obselete code

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,34 +1,31 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 
-import { NavMain } from "@/components/nav-main"
+import { NavMain } from "@/components/nav-main";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
-  SidebarRail
-} from "@/components/ui/sidebar"
-import { navItems } from "@/lib/constants"
-import { Logo } from "./logo"
-import { Separator } from "./ui/separator"
-import { NavUser } from "./nav-user"
-import { usePathname } from "next/navigation"
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { navItems } from "@/lib/constants";
+import { Logo } from "./logo";
+import { Separator } from "./ui/separator";
+import { NavUser } from "./nav-user";
 
 type AppSidebarProps = React.ComponentProps<typeof Sidebar> & {
   user: {
     email: string;
     name: string;
     avatar: string;
-  }
-}
+  };
+};
 
 export function AppSidebar({ user, ...props }: AppSidebarProps) {
-
-
   return (
-    <Sidebar collapsible="icon" {...props} open >
+    <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <Logo />
         <Separator />
@@ -41,5 +38,5 @@ export function AppSidebar({ user, ...props }: AppSidebarProps) {
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
