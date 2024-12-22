893b5e68aa4cce85999341bfbc8260a38e8fe525
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-22 15:52:21 +0500

    feat: implement execution plan hooks - Add validation context integration - Implement error handling - Add execution mutation

--- src/hooks/workflow/use-execution-plan.tsx
+++ src/hooks/workflow/use-execution-plan.tsx
@@ -1,18 +1,59 @@
-
-import { FlowToExecutionPlan } from "@/lib/workflow/execution-plan"
+import { executeWorkflowAction } from "@/actions/workflow/execute-workflow-action"
+import { useFlowValidationContext } from "@/contexts/useFlowValidationContext"
+import { FlowToExecutionPlan, FlowToExecutionPlanType, FlowToExecutionPLanValidationError } from "@/lib/workflow/execution-plan"
 import { AppNodeType } from "@/types/app-node-types"
+import { useMutation } from "@tanstack/react-query"
 import { useReactFlow } from "@xyflow/react"
 import { useCallback } from "react"
+import { toast } from "sonner"
 
 export const useExecutionPlan = () => {
   const { toObject } = useReactFlow()
+  const { setInvalidInputs, clearInputErrors } = useFlowValidationContext()
+
+  const handleError = useCallback((error: FlowToExecutionPlanType["errors"]) => {
+    switch (error?.type) {
+      case FlowToExecutionPLanValidationError.INVALID_INPUTS:
+        setInvalidInputs(error.invalidElements!)
+        break
+      case FlowToExecutionPLanValidationError.MISSING_ENTRY_POINT:
+        clearInputErrors()
+        break
+      default:
+        throw new Error("Unknown error type")
+        break
+    }
+  }, [setInvalidInputs, clearInputErrors])
 
   const generateExecutionPlan = useCallback(() => {
     const { nodes, edges } = toObject()
-    const { executionPlan } = FlowToExecutionPlan(nodes as AppNodeType[], edges)
+    const { executionPlan, errors } = FlowToExecutionPlan(nodes as AppNodeType[], edges)
 
+
+    if (errors) {
+      handleError(errors)
+      return null
+    }
+
+    clearInputErrors()
     return executionPlan
-  }, [toObject])
+  }, [toObject, handleError, clearInputErrors])
 
   return generateExecutionPlan
+}
+
+
+export const useExecutionPlanMutation = () => {
+  const { mutate: executeWorkflow, isPending: isExecuting } = useMutation({
+    mutationFn: executeWorkflowAction,
+    onSuccess: () => {
+      toast.success("Workflow executed successfully")
+    },
+    onError: (error) => {
+      console.error("error", error)
+      toast.error("Error executing workflow")
+    }
+  })
+
+  return { executeWorkflow, isExecuting }
 }
