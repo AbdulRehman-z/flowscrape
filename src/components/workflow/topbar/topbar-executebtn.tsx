691db0970e9f9a401e3a127920ec47120c6764ad
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-22 15:50:39 +0500

    feat: enhance execute button functionality - Add loading state - Implement execution mutation - Update button styling

--- src/components/workflow/topbar/topbar-executebtn.tsx
+++ src/components/workflow/topbar/topbar-executebtn.tsx
@@ -2,28 +2,36 @@
 
 import { TooltipWrapper } from "@/components/tooltip-provider";
 import { Button } from "@/components/ui/button";
-import { useExecutionPlan } from "@/hooks/workflow/use-execution-plan";
+import { useExecutionPlan, useExecutionPlanMutation } from "@/hooks/workflow/use-execution-plan";
 import { useReactFlow } from "@xyflow/react";
-import { PlayIcon } from "lucide-react";
+import { Loader2Icon, PlayIcon } from "lucide-react";
 
 type ExecuteButtonProps = {
   workflowId: string
 }
 
-
 export default function ExecuteButton({ workflowId }: ExecuteButtonProps) {
   const generate = useExecutionPlan()
+  const { toObject } = useReactFlow()
+  const { executeWorkflow, isExecuting } = useExecutionPlanMutation()
 
   function handleExecutionClick() {
     const plan = generate()
-    console.log("--- PLAN ---")
-    console.table(plan)
+    if (!plan) return
+
+    executeWorkflow({
+      workflowId,
+      defination: JSON.stringify(toObject())
+    })
   }
 
   return (
     <TooltipWrapper tooltipContent="Execute workflow">
-      <Button variant={"outline"} size={"icon"} onClick={handleExecutionClick}>
-        <PlayIcon size={24} />
+      <Button variant={"secondary"} onClick={handleExecutionClick} disabled={isExecuting}>
+        {isExecuting ? <Loader2Icon className="animate-spin" /> : <>
+          <PlayIcon size={24} />
+          Execute
+        </ >}
       </Button>
     </TooltipWrapper >
   )
