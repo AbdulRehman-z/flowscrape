b8b18361e244b2daec6db8a1546fa82a67ec2a13
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2024-12-27 21:08:00 +0500

    feat: enhance HTML extraction logging

--- src/lib/workflow/executor/extract-html-from-element-executor.ts
+++ src/lib/workflow/executor/extract-html-from-element-executor.ts
@@ -6,25 +6,31 @@ export async function ExtractHtmlFromElementExecutor(environment: ExecutionEnvir
   try {
     const selector = environment.getInput("Selector")
     if (!selector) {
+      console.error("No selector provided")
       environment.log.info("No selector provided")
       return false
     }
 
     const html = environment.getInput("HTML")
     if (!html) {
+      console.error("No HTML provided")
       environment.log.error("No HTML provided")
       return false
     }
 
     const $ = cheerio.load(html)
     const element = $(selector)
-    if (!element) {
+    console.log({ selector, element, html })
+    if (!element.length) {
+      console.error("No element found")
       environment.log.error("No element found")
       return false
     }
 
     const extractedText = $.text(element)
+    console.log({ extractedText })
     if (!extractedText) {
+      console.error("No text found")
       environment.log.error("No text found")
       return false
     }
