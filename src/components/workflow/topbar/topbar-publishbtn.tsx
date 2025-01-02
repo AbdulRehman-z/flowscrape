83957a23aa999dd7d5c75393f3888c1379880649
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-01-02 22:21:22 +0500

    feat: implement workflow publish button with toast

--- src/components/workflow/topbar/topbar-publishbtn.tsx
+++ src/components/workflow/topbar/topbar-publishbtn.tsx
@@ -1,30 +1,35 @@
 import { TooltipWrapper } from "@/components/tooltip-provider";
 import { Button } from "@/components/ui/button";
-import { useUpdateWorkflow } from "@/hooks/workflow/use-update-workflow";
+import { usePublishWorkflow } from "@/hooks/workflow/use-publish-workflow";
 import { useReactFlow } from "@xyflow/react";
 import { ArrowUpFromLine, LoaderIcon } from "lucide-react";
+import { toast } from "sonner";
 
 type PublishButtonProps = {
   workflowId: string
 }
+
 export default function PublishButton({ workflowId }: PublishButtonProps) {
   const { toObject } = useReactFlow()
-  const { updateWorkflow, isUpdatingWorkflow } = useUpdateWorkflow()
+  // const { updateWorkflow, isUpdatingWorkflow } = useUpdateWorkflow()
+  const { publishWorkflow, isPublishing } = usePublishWorkflow()
 
-  function handleSave() {
+  function handlePublishClick() {
     const defination = JSON.stringify(toObject())
-    updateWorkflow({
+    toast.loading("publishing...", { id: "workflow-publish" })
+
+    publishWorkflow({
       id: workflowId,
-      defination
+      flowDefination: defination,
     })
   }
 
   return (
-    <TooltipWrapper tooltipContent="Save workflow">
-      <Button onClick={handleSave}
-        disabled={!!isUpdatingWorkflow}
+    <TooltipWrapper tooltipContent="Publish workflow">
+      <Button onClick={handlePublishClick}
+        disabled={isPublishing}
       >
-        {isUpdatingWorkflow ? <LoaderIcon className="animate-spin" /> : <>
+        {isPublishing ? <LoaderIcon className="animate-spin" /> : <>
           <ArrowUpFromLine size={20} />
           Publish
         </>}
