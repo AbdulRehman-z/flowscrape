14cbbc8f5c71b2db3ce9e247dfcc4f97fe9ae623
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-01-12 21:12:19 +0500

    Added new task types (FILL_INPUT, CLICK_ELEMENT, WAIT_FOR)

--- src/types/task-type.ts
+++ src/types/task-type.ts
@@ -1,7 +1,10 @@
 export enum TaskTypeEnum {
   LAUNCH_BROWSER = 'LAUNCH_BROWSER',
   PAGE_TO_HTML = 'PAGE_TO_HTML',
-  EXTRACT_TEXT_FROM_ELEMENT = "EXTRACT_TEXT_FROM_ELEMENT"
+  EXTRACT_TEXT_FROM_ELEMENT = "EXTRACT_TEXT_FROM_ELEMENT",
+  FILL_INPUT = 'FILL_INPUT',
+  CLICK_ELEMENT = "CLICK_ELEMENT",
+  WAIT_FOR = "WAIT_FOR"
 }
 
 export enum TaskParamEnum {
@@ -11,6 +14,7 @@ export enum TaskParamEnum {
   BOOLEAN = 'BOOLEAN',
   ARRAY = 'ARRAY',
   OBJECT = 'OBJECT',
+  SELECT = "SELECT"
 }
 
 export type TaskParamType = {
