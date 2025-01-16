8501246143702ea362c112424224cd45bd6401e6
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-01-16 20:00:27 +0500

    feat: update workflow registry with new task executors

--- src/lib/workflow/executions/registary.ts
+++ src/lib/workflow/executions/registary.ts
@@ -1,14 +1,18 @@
 import { ExecutionEnvironment } from "@/types/environment";
 import { type TaskTypeEnum } from "@/types/task-type";
 import { WorkflowTaskType } from "@/types/workflow-types";
+import { AddPropertyToJsonExecutor } from "./add-property-to-json-executor";
 import { ClickElementExecutor } from "./click-element-executor";
 import { DeliverViaWebhookExecutor } from "./deliver-via-webhook-executor";
+import { ExtractDataWithAiExecutor } from "./extract-data-with-ai-executor";
 import { ExtractHtmlFromElementExecutor } from "./extract-html-from-element-executor";
 import { FillInputExecutor } from "./fill-input-executor";
 import { LaunchBrowserExecutor } from "./launch-browser-executor";
 import { PageToHtmlExecutor } from "./page-to-html-executor";
+import { ReadPropertyFromJsonExecutor } from "./read-property-from-json-executor";
 import { WaitForElementExecutor } from "./wait-for-element-executor";
-import { ExtractDataWithAiExecutor } from "./extract-data-with-ai-executor";
+import { NavigateToUrlExecutor } from "./navigate-to-url-executor";
+import { ScrollToElementExecutor } from "./scroll-to-element-executor";
 
 type ExecutorFn<T extends WorkflowTaskType> = (
   environment: ExecutionEnvironment<T>
@@ -30,5 +34,9 @@ export const ExecutorRegistory: RegistryType = {
   CLICK_ELEMENT: ClickElementExecutor,
   WAIT_FOR: WaitForElementExecutor,
   DELIVER_VIA_WEBHOOK: DeliverViaWebhookExecutor,
-  EXTRACT_DATA_WITH_AI: ExtractDataWithAiExecutor
+  EXTRACT_DATA_WITH_AI: ExtractDataWithAiExecutor,
+  READ_PROPERTY_FROM_JSON: ReadPropertyFromJsonExecutor,
+  ADD_PROPERTY_TO_JSON: AddPropertyToJsonExecutor,
+  NAVIGATE_TO_URL: NavigateToUrlExecutor,
+  SCROLL_TO_ELEMENT: ScrollToElementExecutor
 }
